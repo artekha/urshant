643ecbb485681a8ffe2adcb4e412591bc9f4e545
Andrew Artekha <andrewartekha@gmail.com>
2016-04-30 21:56:55 +0300

    first version

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+
+
+gulp.task('sass', function() {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(plumber())
+		.pipe(autoprefixer())
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('sass:watch', function () {
+	gulp.watch('sass/**/*.scss', ['sass']);
+});
